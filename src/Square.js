4a6974d5344419d038dfc8aa14f2a40c3753401a
kantaro.sasajima <kan.0922.baseball@softbank.ne.jp>
2020-12-25 22:33:41 +0900

    ○×

--- src/Square.js
+++ src/Square.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-class Square extends React.Component {
-  render(){
-    return(
-      <button className="square">
-        {this.props.value}
-      </button>
-    )
-  }
+// class Square extends React.Component {
+//   render(){
+//     return(
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick()}
+//       >
+//         {this.props.value}
+//       </button>
+//     )
+//   }
+// }
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 export default Square;
+
+// React では、イベントを表す props には on[Event] という名前、イベントを処理するメソッドには handle[Event] という名前を付けるのが慣習となっています。
